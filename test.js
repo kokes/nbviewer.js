7cfe722d6f7f003d5d9ab98cd24d1d4f4c80e1d4
Ondrej Kokes <ondrej.kokes@gmail.com>
2021-02-11 21:40:21 +0100

    moved TODOs into github's PR

--- test.js
+++ test.js
@@ -1,14 +1,3 @@
-// TODO:
-// rename tests to test_cases?
-// pin marked, katex, prism to the same versions as those CDNed
-// clean up the constructor in nbv.js
-// make the HTML version into a working one
-// set up CI, add more examples etc.?
-// throw exceptions instead of console.errors
-// do not style the main div
-// upload more test cases here
-// walk directory with test cases to test them all
-
 const fs = require("fs");
 
 const marked = require("marked");
