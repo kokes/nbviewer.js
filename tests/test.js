de88506fa1b7ebbaa56f02b47a836c2c32a76662
Ondrej Kokes <ondrej.kokes@gmail.com>
2021-02-16 09:11:16 +0100

    testing in github actions (resolves #52)

--- tests/test.js
+++ tests/test.js
@@ -1,24 +1,40 @@
 const fs = require("fs");
 const path = require("path");
+const process = require("process");
 
 const marked = require("marked");
 const katex = require("katex");
 const prism = require("prismjs");
 
 const { JSDOM } = require("jsdom");
+const { exception } = require("console");
 const document = (new JSDOM("")).window.document;
 const nbv = require("../lib/nbv.js").nbv_constructor(document, {marked, prism, katex});
 
 const target = document.createElement("div");
 
-const nbdir = "notebooks";
+const nbdir = process.argv[2];
 const listing = fs.readdirSync(nbdir)
 
 for (const filename of listing) {
+    if (!filename.endsWith(".ipynb")) {
+        console.log("skipping " + filename);
+        continue
+    }
     const fullPath = path.join(nbdir, filename);
-    const nb = JSON.parse(fs.readFileSync(fullPath));
     console.log(fullPath)
+    const nb = JSON.parse(fs.readFileSync(fullPath));
     nbv.render(nb, target);
+
+    // useful for testing new files:
+    // try {
+    //     nbv.render(nb, target);
+    // } catch(e) {
+    //     console.log(e)
+    //     continue
+    // }
+    // fs.rmSync(fullPath)
+
     // huzzah, our first test
     // console.log(tg.innerHTML)
     // we can compare this to a snapshot
